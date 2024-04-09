8d1c1f3453d616e4323f77c2c4b4e2be8f68f3c7
George <georgevs@gmail.com>
2024-04-09 17:09:20 +0300

    playground pills-rx products

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ```
 openssl enc -aes-128-cbc -pbkdf2 -salt -d -in ~/ws-archive/certs.tar.gz.enc | tar xzv
 ```
-### Start web server in Docker
+### Start node container
 ```
 docker container run --rm \
   --name node-app \
@@ -17,8 +17,21 @@ docker container run --rm \
   --volume "$PWD:/home/node/pills-rx" \
   --publish 3443:3443 \
   -it node bash
-  
+```
+
+### Install dependencies
+```bash
 npm install
+```
+
+### Run with Vite dev server (option #1)
+```bash
 npm run dev -- --host 172.20.0.100 --port 3443
+```
+
+### Run distribution build (option #2)
+```bash
+npm run build
+npx http-server ./dist -c-1 --ssl -a 172.20.0.100 -p 3443 --cert ./certs/cert.pem --key ./certs/cert-key-nopassword.pem
 ```
 Open site at https://xps.spamfro.site:3443 (in LAN) or https://local.spamfro.site:3443 (via proxy: `ssh -L 3443:172.20.0.100:3443 xps`)
--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,124 @@
-import { 
+import { useEffect, useState } from 'react';
+import {
   createBrowserRouter,
   createRoutesFromElements,
   Outlet,
   Route,
   RouterProvider,
-  useParams,
 } from 'react-router-dom';
 
-const App = () => (
-  <RouterProvider router={createAppRouter()} />
-);
+function App() {
+  return (
+    <RouterProvider router={createAppRouter()} />
+  );
+}
 
-const createAppRouter = () => (
-  createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<Layout />} >
-        <Route index element={<HomePage />} />
-        <Route
-          path={'/about/:id'}
-          element={<AboutPage />}
-        />
-      </Route>
+function createAppRouter() {
+  return (
+    createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Prescription />} />
+        </Route>
+      )
     )
-  )
-);
+  );
+}
 
-const Layout = () => (
-  <>
-    <h1>Layout</h1>
-    <Outlet />
-  </>
-);
-
-const HomePage = () => (
-  <>
-    <h2>Home</h2>
-    <XComponent />
-  </>
-);
-
-const AboutPage = () => {
-  const { id } = useParams();
+function Layout() {
   return (
     <>
-      <h2>About {id}</h2>
-      <XComponent />
-      <YComponent />
+      <h1>Pills Rx</h1>
+      <Outlet />
     </>
   );
-};
+}
 
-const XComponent = () => (
-  <p>X Component</p>
-);
+type Product = [id: number, description: string, dosesPerUnit: number];
 
-const YComponent = () => (
-  <p>Y Component</p>
-);
+class Dates {
+  static addDays(dt: Date, n: number): Date {
+    return new Date(dt.getTime() + n * 24 * 60 * 60 * 1000);
+  }
+  static *range(bdt: Date, edt?: Date): Generator<Date> {
+    yield bdt;
+    for (let dt = bdt; edt === undefined || dt < edt; dt = Dates.addDays(dt, 1)) {
+      yield dt;
+    }
+  }
+}
+
+class Format {
+  static day(dt: Date): string { return dt === undefined ? '' : `${dt.getFullYear()}-${String(dt.getMonth() + 1).padStart(2, '0')}-${String(dt.getDate()).padStart(2, '0')}` }
+  static weekDay(dt: Date): string { return dt === undefined ? '' : `${String(dt.getMonth() + 1).padStart(2, '0')}/${String(dt.getDate()).padStart(2, '0')} ${Format.weekday[dt.getDay()]}` }
+  static weekday = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+}
+
+function Prescription() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const startDate = new Date(2024, 3, 9);  // TODO
+  const range = Dates.range(startDate, Dates.addDays(startDate, 14));  // TODO
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchProducts() {
+      const products = await fetch('/data/products.json', { signal: controller.signal })
+        .then(response => {
+          if (!response.ok) { throw new Error(response.statusText) }
+          return response.json();
+        });
+
+      setProducts(products);
+    }
+
+    fetchProducts();
+
+    return () => { controller.abort() };  // AbortError
+  }, []);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={2}></th>
+            <th colSpan={1}>8am</th>
+            <th colSpan={2}>12pm</th>
+            <th colSpan={1}>8pm</th>
+          </tr>
+          <tr>
+            <th colSpan={2}></th>
+            <th>A</th>
+            <th>B</th>
+            <th>C</th>
+            <th>A</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(range).map((dt, day) => {
+            return (
+              <tr key={day}>
+                <td>{day + 1}</td>
+                <td>{Format.weekDay(dt)}</td>
+                <td><input type='checkbox' /></td>
+                <td><input type='checkbox' /></td>
+                <td><input type='checkbox' /></td>
+                <td><input type='checkbox' /></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <h3>Legend</h3>
+      <ol type='A'>
+        {products.map(([id, description]) => (
+          <li key={id}>{description}</li>
+        ))}
+      </ol>
+    </>
+  );
+}
 
 export default App;
